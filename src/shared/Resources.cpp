32329fbaa8f2368f34fd7641f90922ba47071ca1
tinywyrm <hunterdeloe@outlook.com>
2025-11-13 15:19:05 -0500

    texture

--- src/shared/Resources.cpp
+++ src/shared/Resources.cpp
@@ -10,9 +10,19 @@ void LoadTextures(std::string_view path)
 	// parse
 	// create textures from config using SDL_image
 
-	SDL_Surface* surface = SDL_CreateSurface(0, 0, SDL_PIXELFORMAT_RGBA8888);
+	std::string file;
 
+	SDL_Surface* surface = IMG_Load(file.c_str());
+	if (not surface)
+	{
+		SDL_Log("Image loading failed: %s", SDL_GetError());
+	}
 
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(App::Renderer, surface);
+	if (not texture)
+	{
+		SDL_Log("Creating texture failed: %s", SDL_GetError());
+	}
 
 	surface = NULL;
 }
