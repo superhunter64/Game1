48e29a014cf174098816d36a71f4fdfe0b72e9a2
tinywyrm <hunterdeloe@outlook.com>
2025-11-14 17:46:58 -0500

    entity

---
+++ src/components/Character.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Entity.h"
+#include "Sprite.h"
+
+struct Character : Entity
+{
+	AnimatedSprite sprite;
+};
---
+++ src/components/Entity.h
@@ -0,0 +1,12 @@
+#include <SDL3/SDL.h>
+
+struct Transform
+{
+	float x, y;
+	
+};
+
+struct Entity : Transform
+{
+
+};
